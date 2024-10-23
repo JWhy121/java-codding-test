4431504943816496cb24045814a6b1cf87abb95d
[Cloud3] 김연지 <yjuio8357@naver.com>
2024-10-23 20:58:11 +0900

    Initial Commit

---
+++ Codding/BOJ/_s5_7568_덩치.java
@@ -0,0 +1,12 @@
+package Codding.BOJ;
+
+import java.io.*;
+
+public class _s5_7568_덩치 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+    }
+}
