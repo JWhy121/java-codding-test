61cc3c7aff075fa476a578b88833371ebd33065b
[Cloud3] 김연지 <yjuio8357@naver.com>
2024-10-24 21:17:23 +0900

    _s5_7568_덩치

--- Codding/BOJ/_s5_7568_덩치.java
+++ Codding/BOJ/_s5_7568_덩치.java
@@ -5,7 +5,6 @@ import java.io.*;
 public class _s5_7568_덩치 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
 
