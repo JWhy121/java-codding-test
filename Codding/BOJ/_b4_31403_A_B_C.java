9251ec03789ba7a2c0c7ed009208029347d09efa
[Cloud3] 김연지 <yjuio8357@naver.com>
2024-10-24 21:16:42 +0900

    _b4_31403_A+B-C

---
+++ Codding/BOJ/_b4_31403_A_B_C.java
@@ -0,0 +1,24 @@
+package Codding.BOJ;
+
+import java.io.*;
+
+public class _b4_31403_A_B_C {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[3];
+
+        for(int i = 0; i < 3; i++){
+
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        bw.write(String.valueOf(arr[0] + arr[1] - arr[2]) + "\n");
+        bw.write(Integer.parseInt(String.valueOf(arr[0]) + String.valueOf(arr[1])) - arr[2] + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
