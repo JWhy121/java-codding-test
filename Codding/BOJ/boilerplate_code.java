d3484ec595743df5e1417690f785bf0724a4e689
[Cloud3] 김연지 <yjuio8357@naver.com>
2024-11-23 23:04:43 +0900

    bolierplate code

---
+++ Codding/BOJ/boilerplate_code.java
@@ -0,0 +1,21 @@
+package Codding.BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class boilerplate_code {
+
+    private static int T;
+    private static int N;
+    private static int M;
+    private static StringTokenizer st;
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.flush();
+        bw.close();
+
+    }
+}
