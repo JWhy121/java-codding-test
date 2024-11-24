9c88904385637fdfc971628895e80e2d0cc38b55
[Cloud3] 김연지 <yjuio8357@naver.com>
2024-11-25 02:58:35 +0900

    _s2_1874_스택_수열

---
+++ Codding/BOJ/silver/_s2_1874_스택_수열.java
@@ -0,0 +1,21 @@
+package Codding.BOJ.silver;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _s2_1874_스택_수열 {
+
+    private static int T;
+    private static int N;
+    private static int M;
+    private static StringTokenizer st;
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.flush();
+        bw.close();
+
+    }
+}
